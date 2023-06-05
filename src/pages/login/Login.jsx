c9ad16adae772c883287e463d92c954871f152c6
Patryk-Polanski <pat.polanski@gmail.com>
2023-06-05 18:45:25 +0100

    login page: create login form

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,46 @@
+import { useState } from 'react';
+
 import './Login.css';
 
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form className='auth-form' onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type='email'
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type='password'
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button>Login</button>
+      {/* {!isPending ? (
+        <button className='btn'>Login</button>
+      ) : (
+        <button className='btn' disabled>
+          loading...
+        </button>
+      )}
+      {error && <div className='error'>{error}</div>} */}
+    </form>
+  );
 }
