5ff5a080f8bfd8bab0771c8259aa9c68b30929f5
Patryk-Polanski <pat.polanski@gmail.com>
2023-06-09 18:50:41 +0100

    create project form: add custom react-select component for dropdowns

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,6 +1,14 @@
 import { useState } from 'react';
+import Select from 'react-select';
 
 import './Create.css';
+
+const categories = [
+  { value: 'development', label: 'Development' },
+  { value: 'design', label: 'Design' },
+  { value: 'sales', label: 'Sales' },
+  { value: 'marketing', label: 'Marketing' },
+];
 
 export default function Create() {
   const [name, setName] = useState('');
@@ -11,7 +19,7 @@ export default function Create() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(name, details, dueDate);
+    console.log(name, details, dueDate, category.value);
   };
 
   return (
@@ -47,7 +55,10 @@ export default function Create() {
         </label>
         <label>
           <span>Project category:</span>
-          {/* category select here */}
+          <Select
+            options={categories}
+            onChange={(option) => setCategory(option)}
+          />
         </label>
         <label>
           <span>Assign to:</span>
