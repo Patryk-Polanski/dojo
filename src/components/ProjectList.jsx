19ffa92bc8e82163077429ab025cdf288ada429a
Patryk-Polanski <pat.polanski@gmail.com>
2023-06-10 11:11:46 +0100

    dashboard page: create projects grid

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,12 +1,29 @@
+import { Link } from 'react-router-dom';
+
 import './ProjectList.css';
+import Avatar from './Avatar';
 
 export default function ProjectList({ projects }) {
   return (
-    <div>
+    <div className='project-list'>
       {projects.length === 0 ? (
         <p>No projects exist</p>
       ) : (
-        projects.map((project) => <div key={project.id}>{project.name}</div>)
+        projects.map((project) => (
+          <Link to={`/projects/` + project.id} key={project.id}>
+            <h4>{project.name}</h4>
+            <p>Due by {project.dueDate.toDate().toDateString()}</p>
+            <div className='assigned-to'>
+              <ul>
+                {project.assignedUsersList.map((user) => (
+                  <li key={user.id}>
+                    <Avatar src={user.photoURL} />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </Link>
+        ))
       )}
     </div>
   );
